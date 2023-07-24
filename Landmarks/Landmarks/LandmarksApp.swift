9a676f9ef60b8cd15fbcbfda09c05c4341f4f5c2
최수정 <babeeboo2000@gmail.com>
2023-07-25 01:05:36 +0900

    [Init] 프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 어멘드 on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
