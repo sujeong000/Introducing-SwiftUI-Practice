8074872af80071ede18a3963e33b139fc04526e8
최수정 <babeeboo2000@gmail.com>
2023-07-26 20:28:18 +0900

    [Feat] LandmarkList 뷰 생성

---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by 어멘드 on 2023/07/26.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
